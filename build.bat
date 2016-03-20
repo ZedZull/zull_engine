460dbe6cfbffcabbf0403b2635615c53633bbdb7
Jay Waggle <thezedzull@gmail.com>
2016-03-20 11:49:27 -0400

    Fixed build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
 
+if not defined VisualStudioVersion call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+
 set executable_name=zull
 set compiler_flags=/nologo /FC /MT /GR- /EHa- /WX /W4 /wd4100 /wd4996 /wd4244 /wd4702 /wd4189 /Zi /Fe%executable_name% /I..\external\glfw\include /I..\external\LuaJIT-2.0.4\include
 set linker_flags=/LIBPATH:..\external\glfw\lib /LIBPATH:..\external\LuaJIT-2.0.4\lib glfw3.lib lua51.lib opengl32.lib gdi32.lib user32.lib shell32.lib /NODEFAULTLIB:LIBCMTD
