411bbe1272d435e052bd5eb26c6e3bebd012ab89
Jay Waggle <thezedzull@gmail.com>
2016-03-20 11:56:45 -0400

    Fixed build.bat for real

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if not defined VisualStudioVersion call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+if not defined VisualStudioVersion call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
 set executable_name=zull
 set compiler_flags=/nologo /FC /MT /GR- /EHa- /WX /W4 /wd4100 /wd4996 /wd4244 /wd4702 /wd4189 /Zi /Fe%executable_name% /I..\external\glfw\include /I..\external\LuaJIT-2.0.4\include
