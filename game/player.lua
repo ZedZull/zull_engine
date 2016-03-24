b6798af9a074a0c664fcad93a0336754eac7b9f8
darithorn <jedrix2454@gmail.com>
2016-03-23 21:37:12 -0700

    Updated player to new sprite_t

--- game/player.lua
+++ game/player.lua
@@ -1,8 +1,4 @@
-player = sprite_t("game/loveable_rogue.png")
-player_idle = animation_t()
-player_idle:add_frame(rect_t(96, 144, 16, 16), 0)
-player:add_anim("idle", player_idle)
-player:play_anim("idle")
+player = sprite_t("game/loveable_rogue.png", rect_t(96, 144, 16, 16))
 player.x = 100
 player.y = 100
 player.scale = 2
